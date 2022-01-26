485839750008b1f624aecd59d671a4a72c38fb4f
Yared Weldegebrial <yared12707@gmail.com>
2022-01-26 22:18:23 +0300

    minor bug fix.

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default {
 
   async created() {
     axios
-      .get("https://www.fruityvice.com/api/fruit/all")
+      .get("/all")
       .then((response) => {
         this.data = response.data;
         var headerData = response.data[0];
